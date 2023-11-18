066b061f4ebaad470ceb5cb0ab225fe81dfcf61d
RS Shonjoy <85416231+rsshonjoydas@users.noreply.github.com>
2023-11-18 09:31:54 +0600

    feat: server action

---
+++ src/app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import { useFormState } from 'react-dom';
+
+import { create } from '@/actions/create-board';
+import { FormButton } from './form-button';
+import { FormInput } from './form-input';
+
+export const Form = () => {
+  const initialState = { message: null, errors: {} };
+  const [state, dispatch] = useFormState(create, initialState);
+
+  return (
+    <form action={dispatch}>
+      <div className='flex flex-col space-y-2'>
+        <FormInput errors={state?.errors} />
+      </div>
+      <FormButton />
+    </form>
+  );
+};
--- src/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ src/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,9 +1,20 @@
-import { OrganizationSwitcher } from '@clerk/nextjs';
+import { db } from '@/lib/db';
+import { Board } from './board';
+import { Form } from './form';
 
-const OrganizationIdPage = () => (
-  <div>
-    <OrganizationSwitcher hidePersonal />
-  </div>
-);
+const OrganizationIdPage = async () => {
+  const boards = await db.board.findMany();
+
+  return (
+    <div>
+      <Form />
+      <div className='space-y-2'>
+        {boards.map((board) => (
+          <Board key={board.id} id={board.id} title={board.title} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default OrganizationIdPage;
