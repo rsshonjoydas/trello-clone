f9f6421c0ffbab85f01215166b9b2502c69ec184
RS Shonjoy <85416231+rsshonjoydas@users.noreply.github.com>
2023-11-18 17:58:48 +0600

    feat: use action

--- src/app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
+++ src/app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
@@ -1,19 +1,30 @@
 'use client';
 
-import { useFormState } from 'react-dom';
-
-import { create } from '@/actions/create-board';
+import { createBoard } from '@/actions/create-board';
+import { useAction } from '@/hooks/use-action';
 import { FormButton } from './form-button';
 import { FormInput } from './form-input';
 
 export const Form = () => {
-  const initialState = { message: null, errors: {} };
-  const [state, dispatch] = useFormState(create, initialState);
+  const { execute, fieldErrors } = useAction(createBoard, {
+    onSuccess: (data) => {
+      console.log(data, 'SUCCESS');
+    },
+    onError: (error) => {
+      console.log(error, 'ERROR');
+    },
+  });
+
+  const onSubmit = (formData: FormData) => {
+    const title = formData.get('title') as string;
+
+    execute({ title });
+  };
 
   return (
-    <form action={dispatch}>
+    <form action={onSubmit}>
       <div className='flex flex-col space-y-2'>
-        <FormInput errors={state?.errors} />
+        <FormInput errors={fieldErrors} />
       </div>
       <FormButton />
     </form>
