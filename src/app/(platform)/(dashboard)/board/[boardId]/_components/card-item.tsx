200b2506c827804e39ea89903599bf0645c105b1
RS Shonjoy <85416231+rsshonjoydas@users.noreply.github.com>
2023-11-22 10:29:28 +0600

    feat: drag and drop

--- src/app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
+++ src/app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { Draggable } from '@hello-pangea/dnd';
 import { Card } from '@prisma/client';
 
 interface CardItemProps {
@@ -8,10 +9,17 @@ interface CardItemProps {
 }
 
 export const CardItem = ({ data, index }: CardItemProps) => (
-  <div
-    role='button'
-    className='truncate rounded-md border-2 border-transparent bg-white px-3 py-2 text-sm shadow-sm hover:border-black'
-  >
-    {data.title}
-  </div>
+  <Draggable draggableId={data.id} index={index}>
+    {(provided) => (
+      <div
+        {...provided.draggableProps}
+        {...provided.dragHandleProps}
+        ref={provided.innerRef}
+        role='button'
+        className='truncate rounded-md border-2 border-transparent bg-white px-3 py-2 text-sm shadow-sm hover:border-black'
+      >
+        {data.title}
+      </div>
+    )}
+  </Draggable>
 );
